472f6948beed886c610c0b698a0a4757c236e3d5
Cristian Lopez <cristianslhs@gmail.com>
2022-09-18 01:13:18 -0700

    added main functions that allow for 5 rounds. Would like to make it so that each round prompts for a new response from user....

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <script>
 let computerSelection;
 let roundDecision;
 
@@ -13,37 +23,77 @@ function getComputerChoice(min,max) {
         return computerSelection;
 }
 
-getComputerChoice(1,3);
+
 
 
 let answer = prompt('Enter your choice of rock, paper, or scissors.',"");
 let playerSelection = answer.toLowerCase();
 
+let computerCount = 0;
+let playerCount = 0;
+
 function playRound(playerSelection,computerSelection) {
+  computerSelection = getComputerChoice(1,3);
+  
     if ( playerSelection === "rock" && computerSelection ==="rock"){
         roundDecision = "Draw!";
     } else if (playerSelection === "rock" && computerSelection === "paper"){
         roundDecision = "You lose!";
+        computerCount = computerCount + 1;
     } else if (playerSelection === "rock" && computerSelection === "scissors"){
-        roundDecision = "You win!"
+        roundDecision = "You win!";
+        playerCount = playerCount + 1;
     }
     if ( playerSelection === "paper" && computerSelection ==="rock"){
         roundDecision = "You win!";
+        playerCount = playerCount + 1;
     } else if (playerSelection === "paper" && computerSelection === "paper"){
         roundDecision = "Draw!";
     } else if (playerSelection === "paper" && computerSelection === "scissors"){
         roundDecision = "You lose!"
+        computerCount = computerCount +1;
     }
     if ( playerSelection === "scissors" && computerSelection ==="rock"){
         roundDecision = "You Lose!";
+        computerCount = computerCount + 1;
     } else if (playerSelection === "scissors" && computerSelection === "paper"){
         roundDecision = "You Win!";
+        playerCount = playerCount + 1;
     } else if (playerSelection === "scissors" && computerSelection === "scissors"){
         roundDecision = "Draw!"
     } 
-    return roundDecision;
+    return roundDecision,playerSelection,computerSelection, playerCount, computerCount;
+    
 }
 
-playRound(playerSelection,computerSelection);
 
-console.log(playerSelection,computerSelection,roundDecision)
+
+function game(){
+  for (let i = 0; i < 5; i++) {
+   
+   playRound(playerSelection,computerSelection);
+   
+   console.log(playerSelection, computerSelection, roundDecision,playerCount,computerCount);
+  }
+  
+  if (playerCount > computerCount) {
+    console.log("congrats, you win!");
+  } else if (computerCount > playerCount) {
+    console.log("Sorry, you lose!") 
+  } else {
+    console.log("Wow, it's a draw!")
+  }
+ 
+  
+  
+}
+
+game();
+
+
+
+
+
+  </script>
+</body>
+</html>
