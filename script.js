8ea93b7c6e14f20bf60c74b3690f400e2de2e16d
Cristian Lopez <cristianslhs@gmail.com>
2022-10-30 04:34:05 -0700

    working script for RPS! need to do CSS and make it look nice

--- script.js
+++ script.js
@@ -1,100 +1,87 @@
-
-let computerSelection;
-let roundDecision;
 let playerSelection;
+let computerSelection;
 let computerCount = 0;
 let playerCount = 0;
 
-function getComputerChoice(min,max) {
-    let choice = Math.floor(Math.random() * (max - min + 1) + min);
-        if (choice === 1){
-            computerSelection = "rock"
-        } else if (choice === 2){
-            computerSelection = "paper"
-        } else if (choice === 3){
-            computerSelection = "scissors"
-        }
-        return computerSelection;
-}
+buttons = document.getElementById('choices');
+const displayResult = function (message) {
+  document.querySelector('.rounddecision').textContent = message;
+};
 
-
-
-
-function yourTurn(){
-    let answer = prompt('Enter your choice of rock, paper, or scissors.',"");
-    return answer;
-    
-}
-
-function playRound(you,pc) {
-  computerSelection = getComputerChoice(1,3);
-    playerSelection = yourTurn()
-    playerSelection = playerSelection.toLowerCase();
-
-  
-    if ( playerSelection === "rock" && computerSelection ==="rock"){
-        roundDecision = "Draw!";
-    } else if (playerSelection === "rock" && computerSelection === "paper"){
-        roundDecision = "You lose!";
-        computerCount = computerCount + 1;
-    } else if (playerSelection === "rock" && computerSelection === "scissors"){
-        roundDecision = "You win!";
-        playerCount = playerCount + 1;
-    }
-    if ( playerSelection === "paper" && computerSelection ==="rock"){
-        roundDecision = "You win!";
-        playerCount = playerCount + 1;
-    } else if (playerSelection === "paper" && computerSelection === "paper"){
-        roundDecision = "Draw!";
-    } else if (playerSelection === "paper" && computerSelection === "scissors"){
-        roundDecision = "You lose!"
-        computerCount = computerCount +1;
-    }
-    if ( playerSelection === "scissors" && computerSelection ==="rock"){
-        roundDecision = "You Lose!";
-        computerCount = computerCount + 1;
-    } else if (playerSelection === "scissors" && computerSelection === "paper"){
-        roundDecision = "You Win!";
-        playerCount = playerCount + 1;
-    } else if (playerSelection === "scissors" && computerSelection === "scissors"){
-        roundDecision = "Draw!"
-    } else if (playerSelection !== "rock" && 
-    playerSelection !== "paper" && 
-    playerSelection !== "scissors" ) {
-        alert('Please Enter a valid response');
-        playRound();
-    }
-    return roundDecision,playerSelection,computerSelection, playerCount, computerCount;
-    
-}
-
-
-
-function game(){
-    alert('Prepare to enter your choices for 5 rounds of Rock, Paper, Scissors.')
-  while (playerCount !== 5 && computerCount !== 5) {
-   
-   playRound(playerSelection,computerSelection);
-   
-   
-   console.log(playerSelection, computerSelection, roundDecision,playerCount,computerCount);
+// --------------------------------------------
+function getComputerChoice(min, max) {
+  let choice = Math.floor(Math.random() * (max - min + 1) + min);
+  if (choice === 1) {
+    computerSelection = 'rock';
+  } else if (choice === 2) {
+    computerSelection = 'paper';
+  } else if (choice === 3) {
+    computerSelection = 'scissors';
   }
-  
-  if (playerCount > computerCount) {
-    console.log("congrats, you win!");
-  } else if (computerCount > playerCount) {
-    console.log("Sorry, you lose!") 
-  } else {
-    console.log("Wow, it's a draw!")
-  }
- 
-  
-  
+  return computerSelection;
 }
-// test 
-game();
 
+function playRound() {
+  computerSelection = getComputerChoice(1, 3);
 
+  if (playerSelection === 'rock' && computerSelection === 'rock') {
+    roundDecision = 'Draw! 🪨 vs. 🪨';
+  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    roundDecision = 'You lose! Computer picks 🧻';
+    computerCount = computerCount + 1;
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    roundDecision = 'You win! Computer picks ✄';
+    playerCount = playerCount + 1;
+  }
+  if (playerSelection === 'paper' && computerSelection === 'rock') {
+    roundDecision = 'You win! Computer picks 🪨';
+    playerCount = playerCount + 1;
+  } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+    roundDecision = 'Draw! 🧻 vs. 🧻';
+  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    roundDecision = 'You lose! Computer picks ✄';
+    computerCount = computerCount + 1;
+  }
+  if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    roundDecision = 'You Lose! Computer picks 🪨';
+    computerCount = computerCount + 1;
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    roundDecision = 'You Win! Computer picks 🧻';
+    playerCount = playerCount + 1;
+  } else if (
+    playerSelection === 'scissors' &&
+    computerSelection === 'scissors'
+  ) {
+    roundDecision = 'Draw! ✄ vs. ✄';
+  }
+  return (
+    roundDecision,
+    playerSelection,
+    computerSelection,
+    playerCount,
+    computerCount
+  );
+}
 
+buttons.addEventListener('click', event => {
+  playerSelection = event.target.id;
+  playRound(playerSelection, computerSelection);
 
+  if (playerCount === 5) {
+    roundDecision = 'congrats, You beat the computer! 🏆 ';
+  } else if (computerCount === 5) {
+    roundDecision = 'You have lost to the computer.. Try again ❌';
+  }
+  displayResult(roundDecision);
+  document.querySelector('.playerscore').textContent = playerCount;
+  document.querySelector('.computerscore').textContent = computerCount;
 
+  document.querySelector('.reset').addEventListener('click', function () {
+    playerCount = 0;
+    computerCount = 0;
+    document.querySelector('.playerscore').textContent = playerCount;
+    document.querySelector('.computerscore').textContent = computerCount;
+    document.querySelector('.rounddecision').textContent =
+      'Prepare to play a first to five against the computer!';
+  });
+});
